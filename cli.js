8063d1cd085cbe317ae6589584cc41fc3438ef8b
Stef de Goey <stefdegoey@outlook.com>
2020-04-24 12:13:49 +0200

    Added includeHtml flag to the CLI command, fixes meedan/i18n-translate-json#3 Including an example and yargs for the CL

--- cli.js
+++ cli.js
@@ -6,28 +6,37 @@ var translate = require("./translate");
 var path = require("path");
 var getJSON = require("get-json");
 
-var args = process.argv;
+var argv = require("yargs")
+    .usage("Usage: $0 <apiKey> <startDir> <sourceLang> <targetLang1,targetLang2,..>")
+    .boolean("includeHtml")
+    .describe("includeHtml", "Include HTML entries in the translation")
+    .help("h")
+    .alias("h", "help")
+    .example("$0 YOUR_API_KEY examples/ en nl,de", "Translate the file examples/en.json to examples/nl.json and examples/de.json")
+    .argv;
 
-if (args.length < 5) {
-	throw "not enough arguments: i18n-translate apiKey startDir sourceLang (targetLang1,targetLang2,..) (file1,file2,..)";
+if (argv._.length < 4) {
+  console.error("Usage: i18-translate-json <apiKey> <startDir> <sourceLang> <targetLang1,targetLang2,..>");
+  return 1;
 }
 
 // get the start directory from parameters
-var apiKey = args[2];
-var startDir = args[3];
-var sourceLang = args[4];
-var targetLang = args[5];
+var apiKey = argv._[0];
+var startDir = argv._[1];
+var sourceLang = argv._[2];
+var targetLang = argv._[3];
 
 // append / at the end of directory
 if (startDir[startDir.length - 1] != "/") {
 	startDir += "/";
 }
 
+
 // run translation
 var run = function() {
   path.resolve(__dirname, startDir);
 
-  translate.run(apiKey, startDir, sourceLang, targetLang, function(err, result) {
+  translate.run(apiKey, startDir, sourceLang, targetLang, argv.includeHtml, function(err, result) {
   
   	if (err) {
   		console.log("ERROR:");
